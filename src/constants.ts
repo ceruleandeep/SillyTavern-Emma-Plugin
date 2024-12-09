d4e3304d62a63cbd81ef79f0cb7b6a4a7ab75352
ceruleandeep <cerulean@cerulean.foo>
2024-12-09 21:01:42 +1100

    Constancy

---
+++ src/constants.ts
@@ -0,0 +1,7 @@
+export const PUBLIC_DIRECTORIES = {
+    images: 'public/img/',
+    backups: 'backups/',
+    sounds: 'public/sounds',
+    extensions: 'public/scripts/extensions',
+    globalExtensions: 'public/scripts/extensions/third-party',
+};
